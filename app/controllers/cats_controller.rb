70405f7461c86533a5f847b1f6262b683057edd8
App Academy Student <appacademy@Apps-Mac-mini-9.local>
2015-10-20 15:46:21 -0400

    Create #edit and #new with _form partial.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -12,7 +12,38 @@ class CatsController < ApplicationController
   end
 
   def new
+    @cat = Cat.new
     render :new
   end
 
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to cat_url(@cat.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+  end
 end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -3,6 +3,7 @@ require 'date'
 class Cat < ActiveRecord::Base
   validates :sex, inclusion: { in: %w(M F),
     message: "must be M or F."}
+  validates :birth_date, :name, :color, :sex, :description, presence: true
 
   def age
     (Date.today.year - self.birth_date.year).to_i
